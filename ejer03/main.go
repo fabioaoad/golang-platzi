35b858b8619d323ed2ce65c1ecd68baeb85021ae
Fabio <fabioaoad@gmail.com>
2023-03-08 19:13:42 -0300

    for condicional, for while, for forever

---
+++ ejer03/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// for condicional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Printf("\n")
+
+	// for while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// for forever
+	//counterForever := 0
+	//for {
+	//	fmt.Println(counterForever)
+	//	counterForever++
+	//	}
+	fmt.Printf("\n")
+
+	// reto: iniciar en 10 e ir imprimiendo a medida que se decrementa el indice
+	for i := 10; i > 0; i-- {
+		fmt.Println(i)
+	}
+}
